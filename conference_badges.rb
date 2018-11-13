af234a90622aee3d5f6c2fd8dc1d2e39333c2e9b
Kiley Bobbitt <jkileybob@aol.com>
2018-11-13 23:18:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,5 +21,17 @@ def assign_rooms(name_array)
   return room_array
 end
 
-def print()
-end  
+def printer (array1)
+array2 = []
+  array1.each do |element|
+    array2.push(badge_maker(element))
+  end  
+  counter = 1
+  array1.each do |element|
+    array2.push("Hello, #{element}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  
+  array2.each do |element|
+    puts element
+  end
