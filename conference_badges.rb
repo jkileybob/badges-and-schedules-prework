fa1d4807a33b129432fba8f6e14a6276ee92b2de
Kiley Bobbitt <jkileybob@aol.com>
2018-11-13 23:08:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,10 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
-def batch_badge_creator(attendees)
-  return attendees
+def batch_badge_creator(name_array)
+  badge_array = []
+  name_array.each do |element|
+    badge_array.push(badge_maker(element)) 
+  end
+  return badge_array  
 end
